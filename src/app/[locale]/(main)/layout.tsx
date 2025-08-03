b97ba17381ae75322e4e2998b24b42502c0e1a17
Zainulobiddin <zainiddin893@gmail.com>
2025-08-04 00:36:40 +0500

    delete component MiniDrawer in page layout.tsx

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -1,5 +1,4 @@
 'use client';
-import MiniDrawer from "@/components/layout/side-bar/side-bar";
 import ProtectedRoute from "@/components/protectedRoute/protectedRoute";
 import ResponsiveNav from "@/components/ResponsiveNav/ResponsiveNav";
 import { Box } from "@mui/material";
@@ -10,11 +9,9 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   return (
     <ProtectedRoute>
       <ResponsiveNav>
-        <MiniDrawer>
           <Box sx={{ display: "flex" }}>
             {children}
           </Box>
-        </MiniDrawer>
       </ResponsiveNav>
     </ProtectedRoute>
   );
